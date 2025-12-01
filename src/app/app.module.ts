6721eee838e9d9db4db4e84a0c7ee4c9ebc527af
vladkolistratov89-lab <vladkolistratov89@gmail.com>
2025-12-01 10:01:31 +0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-// src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
 import { ItemFormComponent } from './components/item-form/item-form.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent,
-    ItemFormComponent
+    ItemFormComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
